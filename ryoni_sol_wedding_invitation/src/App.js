51208a83f6013ac5ef8fe061c835f38b1d9cfefa
RyoniCho <carpinus0@gmail.com>
2022-06-23 14:15:23 +0900

    ReactJS app first commit

---
+++ ryoni_sol_wedding_invitation/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {Component} from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         Wedding Invitation Start
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
