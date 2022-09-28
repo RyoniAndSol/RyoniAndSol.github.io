966cf3d3ee437cc4b9e51e46d2ec950157e67e5c
RyoniCho <carpinus0@gmail.com>
2022-09-28 16:24:42 +0900

    Gallery Full screen button on

--- ryoni_sol_wedding_invitation/src/Components/Gallery.js
+++ ryoni_sol_wedding_invitation/src/Components/Gallery.js
@@ -72,7 +72,7 @@ const Gallery = () => {
       <Divider plain style={{ marginTop: 0, marginBottom: 32 }}>
         <Title>Gallery</Title>
       </Divider>
-      <ImageGallery showPlayButton={false} showFullscreenButton={false} items={images} />
+      <ImageGallery showPlayButton={false} showFullscreenButton={true} items={images} />
      
     </Wrapper>
    
